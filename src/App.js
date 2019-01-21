dbc56473680c2b9de6a4f4409f7d35b7c357a30d
Sun5hine <af5kh@yahoo.com>
2019-01-21 20:53:10 +0000

    changes

--- src/App.js
+++ src/App.js
@@ -7,8 +7,8 @@ class App extends Component {
     return (
       <div className="App">
        <h1> testing now 11</h1>
-       <h2> Gotta know better</h2>
-        <h3> testing !</testing>
+       
+        <h3> testing !</h3>
       </div>
     );
   }
